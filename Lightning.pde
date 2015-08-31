0ab200be12b26eaa26384b28d0d107dca6b845c2
darrenapcs <lgytuan@gmail.com>
2015-08-31 11:46:39 -0700

    Lalala

--- Lightning.pde
+++ Lightning.pde
@@ -1,10 +1,20 @@
+int startX = 0;
+int startY = 150;
+int endX = 0;
+int endY = 150;
+
 void setup()
 {
   size(300,300);
+  strokeWeight(5);
+  background(0);
+
 }
 void draw()
 {
-
+ stroke((int)(Math.random()*256));
+ while(endX < 300)
+ 	endX = startX + (int)(Math.random*9);
 }
 void mousePressed()
 {
---
+++ build-tmp/source/Lightning.java
@@ -0,0 +1,46 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Lightning extends PApplet {
+
+int startX = 0;
+int startY = 150;
+int endX = 0;
+int endY = 150;
+
+public void setup()
+{
+  size(300,300);
+  strokeWeight(5);
+  background(0);
+
+}
+public void draw()
+{
+ stroke((int)(Math.random()*256));
+}
+public void mousePressed()
+{
+
+}
+
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Lightning" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
