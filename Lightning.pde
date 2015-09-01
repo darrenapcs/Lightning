0a4f5c1d3dff935823dfe471def12d818c7ad268
darrenapcs <lgytuan@gmail.com>
2015-09-01 10:32:47 -0700

    la

--- Lightning.pde
+++ Lightning.pde
@@ -6,18 +6,27 @@ int endY = 150;
 void setup()
 {
   size(300,300);
-  strokeWeight(5);
+  strokeWeight(1);
   background(0);
 
 }
 void draw()
 {
- stroke((int)(Math.random()*256));
- while(endX < 300)
- 	endX = startX + (int)(Math.random*9);
+ 
+ while(endX < 300){
+ 	endX = startX + (int)(Math.random()*9);
+ 	stroke((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
+    endY = startY + (int)(Math.random()*18-9);
+    line(startX,startY,endX,endY);
+    startX = endX;
+    startY = endY;
+}
 }
 void mousePressed()
 {
-
+startX = 0;
+startY = 150;
+endX = 0;
+endY = 150;
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -22,17 +22,28 @@ int endY = 150;
 public void setup()
 {
   size(300,300);
-  strokeWeight(5);
+  strokeWeight(1);
   background(0);
 
 }
 public void draw()
 {
- stroke((int)(Math.random()*256));
+ 
+ while(endX < 300){
+ 	endX = startX + (int)(Math.random()*9);
+ 	stroke((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
+    endY = startY + (int)(Math.random()*18-9);
+    line(startX,startY,endX,endY);
+    startX = endX;
+    startY = endY;
+}
 }
 public void mousePressed()
 {
-
+startX = 0;
+startY = 150;
+endX = 0;
+endY = 150;
 }
 
   static public void main(String[] passedArgs) {
